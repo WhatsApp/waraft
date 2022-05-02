4c25f412a399457dd4b0dea297c207dd88be757d
Henry Sun <hsun324@fb.com>
2022-05-02 15:39:22 -0700

    add RAFT server state to wa_raft_info
    Summary:
    Add the state of the RAFT server to `wa_raft_info` for easy asynchronous
    access even in cases where the RAFT server is busy serving a prior request.
    
    The `gen_server` should still call the module's `terminate` callback even
    when the server stops due to a crash so it is enough to just clear the
    server state entry in the `terminate` callback.
    
    Reviewed By: jaher
    
    Differential Revision: D36020350
    
    fbshipit-source-id: 85ebb9a698e5b9f04969228bcd8b72c735f4dfbd

--- src/wa_raft_info.erl
+++ src/wa_raft_info.erl
@@ -11,6 +11,9 @@
 
 -export([
     init_tables/0,
+    set_state/3,
+    get_state/2,
+    delete_state/2,
     set_leader/3,
     get_leader/2,
     set_stale/2,
@@ -21,6 +24,8 @@
 
 %% Raft leader node
 -define(RAFT_LEADER_NODE(Table, Partition), {leader, Table, Partition}).
+%% Raft server state
+-define(RAFT_SERVER_STATE(Table, Partition), {state, Table, Partition}).
 %% Raft stale flag
 -define(RAFT_STALE_FLAG(Table, Partition), {stale, Table, Partition}).
 
@@ -31,27 +36,44 @@ init_tables() ->
 
 -spec set_leader(wa_raft:table(), wa_raft:partition(), node()) -> true.
 set_leader(Table, Partition, Value) ->
-    ets:update_element(?MODULE, ?RAFT_LEADER_NODE(Table, Partition), {2, Value}) orelse ets:insert(?MODULE, {?RAFT_LEADER_NODE(Table, Partition), Value}).
+    ets:update_element(?MODULE, ?RAFT_LEADER_NODE(Table, Partition), {2, Value})
+        orelse ets:insert(?MODULE, {?RAFT_LEADER_NODE(Table, Partition), Value}).
 
 -spec get_leader(wa_raft:table(), wa_raft:partition()) -> node() | undefined.
 get_leader(Table, Partition) ->
-    try ets:lookup_element(?MODULE, ?RAFT_LEADER_NODE(Table, Partition), 2) of
-        Leader ->
-            Leader
+    try
+        ets:lookup_element(?MODULE, ?RAFT_LEADER_NODE(Table, Partition), 2)
     catch error:badarg ->
         undefined
     end.
 
+-spec set_state(wa_raft:table(), wa_raft:partition(), wa_raft_server:state()) -> true.
+set_state(Table, Partition, State) ->
+    ets:update_element(?MODULE, ?RAFT_SERVER_STATE(Table, Partition), {2, State})
+        orelse ets:insert(?MODULE, {?RAFT_SERVER_STATE(Table, Partition), State}).
+
+-spec get_state(wa_raft:table(), wa_raft:partition()) -> wa_raft_server:state() | undefined.
+get_state(Table, Partition) ->
+    try
+        ets:lookup_element(?MODULE, ?RAFT_SERVER_STATE(Table, Partition), 2)
+    catch error:badarg ->
+        undefined
+    end.
+
+-spec delete_state(wa_raft:table(), wa_raft:partition()) -> true.
+delete_state(Table, Partition) ->
+    ets:delete(?MODULE, ?RAFT_SERVER_STATE(Table, Partition)).
+
 %% Set to true if data on current node is stale. Read on this node may return out-of-dated data
 -spec set_stale(boolean(), #raft_state{}) -> true.
 set_stale(Stale, #raft_state{table = Table, partition = Partition}) ->
-    ets:update_element(?MODULE, ?RAFT_STALE_FLAG(Table, Partition), {2, Stale}) orelse ets:insert(?MODULE, {?RAFT_STALE_FLAG(Table, Partition), Stale}).
+    ets:update_element(?MODULE, ?RAFT_STALE_FLAG(Table, Partition), {2, Stale})
+        orelse ets:insert(?MODULE, {?RAFT_STALE_FLAG(Table, Partition), Stale}).
 
 -spec get_stale(wa_raft:table(), wa_raft:partition()) -> boolean().
 get_stale(Table, Partition) ->
-    try ets:lookup_element(?MODULE, ?RAFT_STALE_FLAG(Table, Partition), 2) of
-        Stale ->
-            Stale
+    try
+        ets:lookup_element(?MODULE, ?RAFT_STALE_FLAG(Table, Partition), 2)
     catch error:badarg ->
         true
     end.
