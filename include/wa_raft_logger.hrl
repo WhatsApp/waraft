1fd869ee59632027f9540421d2eadf2085fe9132
Adarsh Patel <adarshp@meta.com>
2025-07-14 10:37:50 -0700

    add copyright header to file
    Summary: `wa_raft_header.hrl` was missing a copyright header, which opened this task T230770473
    
    Reviewed By: jaher
    
    Differential Revision: D78280624
    
    fbshipit-source-id: 06de8e59c8d0cd3d18d825a7940506b3952cd719

--- include/wa_raft_logger.hrl
+++ include/wa_raft_logger.hrl
@@ -1,3 +1,9 @@
+%%% Copyright (c) Meta Platforms, Inc. and affiliates. All rights reserved.
+%%%
+%%% This source code is licensed under the Apache 2.0 license found in
+%%% the LICENSE file in the root directory of this source tree.
+%%%
+
 -include_lib("kernel/include/logger.hrl").
 
 -define(RAFT_LOG_OPTS, #{domain => [whatsapp, wa_raft]}).
